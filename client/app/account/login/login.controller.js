d0a5bca3c92b37bdbc27ba7cb155b7cb8fd15268
Fernando Souza <nandosouzafilho@gmail.com>
2015-08-08 23:26:04 -0300

    #5 Redirect to admin page after login

--- client/app/account/login/login.controller.js
+++ client/app/account/login/login.controller.js
@@ -5,25 +5,32 @@ angular.module('nodecmsApp')
     $scope.user = {};
     $scope.errors = {};
 
-    $scope.login = function(form) {
+    function login(form) {
+      Auth.login({
+        email: $scope.user.email,
+        password: $scope.user.password
+      })
+      .then( function() {
+        // Logged in, redirect to home
+        $location.path('/admin');
+      })
+      .catch( function(err) {
+        $scope.errors.other = err.message;
+      });
+    };
+
+    function loginHandleSubmit(form){
       $scope.submitted = true;
 
       if(form.$valid) {
-        Auth.login({
-          email: $scope.user.email,
-          password: $scope.user.password
-        })
-        .then( function() {
-          // Logged in, redirect to home
-          $location.path('/');
-        })
-        .catch( function(err) {
-          $scope.errors.other = err.message;
-        });
+        login();
       }
     };
 
-    $scope.loginOauth = function(provider) {
+    function loginOauth(provider) {
       $window.location.href = '/auth/' + provider;
     };
+
+    $scope.loginHandleSubmit = loginHandleSubmit;
+    $scope.loginOauth = loginOauth;
   });
